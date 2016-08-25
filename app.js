0f4e26f0b718844030336e8841f4af3b7d14f591
alaint16 <atovar@optimeconsulting.com>
2016-08-25 09:19:53 -0400

    3 commit
    worked on responsive index page, and the js file for the input button

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-    $.get("https://www.googleapis.com/youtube/v3/search?part=id&q=surf&type=video&key=AIzaSyC87Q6MWVTUKC-ycJOXwWF6l4FXrg2ikxQ", function(data) {
+  //1. get the value of the input
+  //2. get the click event of the button
+  //3. call these two api inside the event
+  //4. change the url and put the value of the input
+var fbs = "popular";
+$(".btn_fb").click(function(){
+    fbs = $(".fbsearch").val()
+    console.log(fbs);
+
+
+
+    $.get("https://www.googleapis.com/youtube/v3/search?part=id&q="+fbs+"&type=video&key=AIzaSyC87Q6MWVTUKC-ycJOXwWF6l4FXrg2ikxQ", function(data) {
         // console.log(data);
         for (var i = 0; i < data.items.length; i++) {
             // console.log(data.items[i].id.videoId)
@@ -8,6 +19,11 @@ $(document).ready(function() {
 
         }
     })
+
+});
+
+
+
     $.get("http://content.guardianapis.com/search?q=surf&format=json&order-by=relevance&api-key=298b3cb0-43ef-4213-8225-606e2b554039", function(news) {
         console.log(news);
         for (var i = 0; i < news.response.results.length; i++) {
